c36b814615c6a95a9b9a650eb97921f7118cd8b6
Zach Bennett <xslingshot@gmail.com>
2017-09-14 20:49:08 -0400

    Remove Mute button from Splash screen.
    Signed-off-by: Zach Bennett <xslingshot@gmail.com>

--- src/states/splash.js
+++ src/states/splash.js
@@ -1,18 +1,13 @@
 define([
-    'phaser',
-    'mute-button'
-], function (Phaser, MuteButton) { 
+    'phaser'
+], function (Phaser) { 
     'use strict';
 
     // Shortcuts
     var game,
 
         title,
-        playBtn,
-
-        muteBtn,
-
-        music;
+        playBtn;
 
     return {
         // Intro
@@ -26,7 +21,7 @@ define([
             title = game.add.sprite(game.width / 2, 0, 'title');
             title.anchor.set(0.5, 0);
 
-            // Credits button
+            // Play button
             playBtn = game.add.button(game.width / 2, game.height - 20, 'tap-to-play-btn', this.onPlayBtnClicked);
             playBtn.anchor.set(0.5, 1);
             playBtn.animations.add('selected', [0,1,0], 20);
@@ -35,10 +30,7 @@ define([
             playBtn.animations.add('blink-on', [5,4,0], 30);
             this.blinkOff();
 
-            game.input.onDown.add(this.onPlayBtnClicked, this); 
-
-            // Mute button
-            muteBtn = game.add.existing(new MuteButton(game));
+            game.input.onDown.add(this.onPlayBtnClicked, this);
         },
 
         blinkOn: function() {
@@ -52,11 +44,9 @@ define([
         },
 
         onPlayBtnClicked: function() {
-            if (!muteBtn.input.pointerOver()) {
-                playBtn.animations.play('selected').onComplete.addOnce(function() {
-                    this.game.stateTransition.to('Menu', true, false);
-                }, this);
-            }
+            playBtn.animations.play('selected').onComplete.addOnce(function() {
+                this.game.stateTransition.to('Menu', true, false);
+            }, this);
         }
     };
 });
