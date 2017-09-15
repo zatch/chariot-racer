5e96721a6b8354cf94ab6cd0a1f23417b0a4b96b
Zach Bennett <xslingshot@gmail.com>
2017-09-15 00:04:02 -0400

    Editor: remove unused maxTokens block from writer.php.
    Signed-off-by: Zach Bennett <xslingshot@gmail.com>

--- editor/writer.php
+++ editor/writer.php
@@ -9,21 +9,6 @@
 $catch = file_get_contents('php://input');
 $input = json_decode($catch,true);
 
-$outputstring = '';
-foreach ($input['levels'] as $i=>$level){
-    $tokenCount = 0;
-    foreach ($level['patterns'][0]['sets'] as $set){
-        foreach($set['lanes'] as $lane){
-            foreach($lane as $value){
-                if(isset($value['key'])&& ( $value['key']=='token' || $value['key']=='power-up')){
-                    $tokenCount++;
-                }
-            }
-        }
-    }
-    $input['levels'][$i]['tokenCount']=$tokenCount;
-}
-// $outputstring = 'var levels = '.json_encode($input['levels']);
 $outputstring = ''
 .'define([], function () { '
 .'"use strict";'
