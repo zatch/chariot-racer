0fea271acc0362d903cfbe004c78d02e4722edfc
sroehrl <sroehrl@eagleproductivity.com>
2017-09-17 19:31:51 -0400

    copy, move patterns & sets

--- editor/directives.js
+++ editor/directives.js
@@ -1,1 +1,26 @@
-app.directive('');
+app.directive('move',function($mdDialog){
+    return{
+        restrict:'E',
+        scope:{
+            levels:'@'
+
+        },
+        template:'<div layout-padding><h2>Select</h2>' +
+        '<md-select ng-model="mover">' +
+        '<md-option value="{{k.level}}"  ng-repeat="k in dropdown track by $index">{{$index}}</md-option>' +
+        '</md-select><md-button ng-click="move()">ok</md-button><md-button ng-click="cancel()">cancel</md-button></div>',
+        link:function(scope){
+            scope.dropdown=[];
+            for(var i=0; i<scope.levels;i++){
+                scope.dropdown.push({level:i});
+            }
+            scope.move = function(){
+                $mdDialog.hide(scope.mover);
+            };
+            scope.cancel = function(){
+                $mdDialog.cancel();
+            }
+
+        }
+    }
+});
